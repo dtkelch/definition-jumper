01277480a7fed3ad3905fe85088199d604f51b71
Dan Kelch <dan.kelch@atomicobject.com>
2018-01-05 09:21:47 -0500

    Update icons

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 'use strict';
 import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
-    let jumpTo = vscode.commands.registerCommand('extension.jumpTo', () => {
+    const jumpTo = vscode.commands.registerCommand('extension.jumpTo', () => {
         vscode.commands.executeCommand('editor.action.goToDeclaration')
    });
-    let jumpBack = vscode.commands.registerCommand('extension.jumpBack', () => {
+    const jumpBack = vscode.commands.registerCommand('extension.jumpBack', () => {
         vscode.commands.executeCommand('workbench.action.navigateBack')
    });
     context.subscriptions.push(jumpTo, jumpBack);
